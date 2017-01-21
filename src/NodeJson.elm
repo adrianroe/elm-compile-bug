66aafb18076e2cdfec935f6864a3594db7c380bd
Adrian Roe <adrian@id3as.co.uk>
2017-01-21 11:21:27 +0100

    return to working state

--- src/NodeJson.elm
+++ src/NodeJson.elm
@@ -5,13 +5,13 @@ import Json.Decode exposing (string, list, lazy, Decoder)
 import Json.Decode.Pipeline exposing (decode, required)
 
 
+nodesDecoder : Decoder Nodes
+nodesDecoder =
+    Json.Decode.map Nodes (list (lazy (\_ -> decodeNode)))
+
+
 decodeNode : Decoder Node
 decodeNode =
     decode Node
         |> required "name" string
         |> required "nodes" nodesDecoder
-
-
-nodesDecoder : Decoder Nodes
-nodesDecoder =
-    Json.Decode.map Nodes (list (lazy (\_ -> decodeNode)))
